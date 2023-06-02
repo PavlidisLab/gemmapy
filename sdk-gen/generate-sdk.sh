827fa56be872adb1af29ff61783a34bdd0dee553
OganM <ogan.mancarci@gmail.com>
2023-06-02 16:34:42 -0700

    set useragent to gemmapy

--- sdk-gen/generate-sdk.sh
+++ sdk-gen/generate-sdk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 temp_sdk_dir=$(mktemp -d)
 echo "SDK will be generated in $temp_sdk_dir..."
-java -jar swagger-codegen-cli.jar generate -i openapi.yaml -l python -t templ -DpackageName=gemmapy.sdk -o "$temp_sdk_dir"
+java -jar swagger-codegen-cli.jar generate -i openapi.yaml -l python -t templ -DpackageName=gemmapy.sdk -o "$temp_sdk_dir" --http-user-agent gemmapy/0.0.2
 rsync -av --delete "$temp_sdk_dir/gemmapy/sdk/" ../gemmapy/sdk/
