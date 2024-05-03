bb2f231e31cbd49f65719eddad2e058721e7278d
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2024-05-03 11:35:59 -0700

    Few improvements for the generate-sdk.sh script
    Use variables for software versions.
    
    Retrieve gemmapy version from setup.cfg.

--- sdk-gen/generate-sdk.sh
+++ sdk-gen/generate-sdk.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+swagger_version=3.0.55
+gemmapy_version=0.0.2
 temp_sdk_dir=$(mktemp -d)
 script_dir=$(dirname "${BASH_SOURCE[0]}")
-curl https://gemma.msl.ubc.ca/rest/v2/openapi.yaml -o openapi.yaml --compressed
-curl https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.55/swagger-codegen-cli-3.0.55.jar -o swagger-codegen-cli.jar
+echo "Updating openapi.yaml..."
+curl https://gemma.msl.ubc.ca/rest/v2/openapi.yaml -o "$script_dir/openapi.yaml" --compressed
+echo "Updating swagger-codegen-cli.jar..."
+curl "https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/$swagger_version/swagger-codegen-cli-$swagger_version.jar" -o swagger-codegen-cli.jar
 
 echo "SDK will be generated in $temp_sdk_dir..."
-java -jar swagger-codegen-cli.jar generate -i "$script_dir/openapi.yaml" -l python -t "$script_dir/templ" -DpackageName=gemmapy.sdk -o "$temp_sdk_dir" --http-user-agent gemmapy/0.0.2
+java -jar swagger-codegen-cli.jar generate -i "$script_dir/openapi.yaml" -l python -t "$script_dir/templ" -DpackageName=gemmapy.sdk -o "$temp_sdk_dir" --http-user-agent "gemmapy/$gemmapy_version"
 rsync -av --delete "$temp_sdk_dir/gemmapy/sdk/" "$script_dir/../gemmapy/sdk/"
