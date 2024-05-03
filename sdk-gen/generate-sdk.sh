a59b5fdb04714dfda1ca83357afbcf361f36d702
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2024-05-03 11:24:10 -0700

    Update swagger-codegen-cli to 3.0.55

--- sdk-gen/generate-sdk.sh
+++ sdk-gen/generate-sdk.sh
@@ -2,7 +2,7 @@
 temp_sdk_dir=$(mktemp -d)
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 curl https://gemma.msl.ubc.ca/rest/v2/openapi.yaml -o openapi.yaml --compressed
-curl https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.35/swagger-codegen-cli-3.0.35.jar -o swagger-codegen-cli.jar
+curl https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.55/swagger-codegen-cli-3.0.55.jar -o swagger-codegen-cli.jar
 
 echo "SDK will be generated in $temp_sdk_dir..."
 java -jar swagger-codegen-cli.jar generate -i "$script_dir/openapi.yaml" -l python -t "$script_dir/templ" -DpackageName=gemmapy.sdk -o "$temp_sdk_dir" --http-user-agent gemmapy/0.0.2
