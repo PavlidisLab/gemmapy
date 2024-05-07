e0767368145adc1bef65b0b20919a5a3f29ad16f
OganM <ogan.mancarcii@gmail.com>
2024-05-07 14:03:27 -0700

    fix subprocessor module name

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,14 +8,13 @@ Created on Tue Mar  5 18:06:47 2024
 import pytest
 import gemmapy
 import pandas as pd
-from gemmapy import subprocessors as sub
+from gemmapy import _subprocessors as sub
 import anndata as ad
 
 
 api = gemmapy.GemmaPy()
 
 def test_get_result_sets():
-    res = api.get_result_sets([200])
     res = api.get_result_sets([200])
     
     assert type(res) is pd.core.frame.DataFrame
