694ae15015d6b1a6bf996642b35a179daf6cc4bd
OganM <ogan.mancarcii@gmail.com>
2025-05-28 12:00:56 -0700

    version bump

--- gemmapy/sdk/api_client.py
+++ gemmapy/sdk/api_client.py
@@ -73,7 +73,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'gemmapy/1.0.7'
+        self.user_agent = 'gemmapy/2.0.0'
 
     def __del__(self):
         self.pool.close()
